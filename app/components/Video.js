8ee58f6edadf75a047d295b7ee81af99599b111f
ellielsewhere <114022661+ellielsewhere@users.noreply.github.com>
2023-07-14 16:35:21 -0500

    css

--- app/components/Video.js
+++ app/components/Video.js
@@ -5,16 +5,24 @@ import Image from 'next/image';
 const Video = () => {
     return (
         <div className="video-container">
-            <Image width={700} height={540} className="home-logo" src={"/img/NEWRELEASE3.png"} alt="" />
-            <iframe
-                className="iframe-video"
-                width={400}
-                height={530}
-                src="https://www.youtube.com/embed/CTw1VFeYnHM?autoplay=1&loop=1&mute=1&controls=0&playlist=CTw1VFeYnHM&showinfo=0&rel=0&enablejsapi=1"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;"
-                allowFullScreen
-            ></iframe>
+            <div style={{ position: "relative", width: "100%", height: "540px" }}>
+                <Image
+                    layout="fill"
+                    className="home-logo"
+                    objectFit="fill"
+                    src={"/img/NEWRELEASE3.png"}
+                    alt=""
+                />
+                <iframe
+                    className="iframe-video"
+                    width={500}
+                    height={530}
+                    src="https://www.youtube.com/embed/CTw1VFeYnHM?autoplay=1&loop=1&mute=1&controls=0&playlist=CTw1VFeYnHM&showinfo=0&rel=0&enablejsapi=1"
+                    title="YouTube video player"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;"
+                    allowFullScreen
+                ></iframe>
+            </div>
         </div>
     );
 };

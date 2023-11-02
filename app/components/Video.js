7bb29209a4ff15d46d74ebd2585fd1f974fad196
ellielsewhere <114022661+ellielsewhere@users.noreply.github.com>
2023-11-02 12:13:54 -0500

    saving

--- app/components/Video.js
+++ app/components/Video.js
@@ -1,16 +1,8 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import "../styles/video.css";
+import Image from "next/image";
 
 const Video = () => {
-  const videoRef = useRef(null);
-  const containerRef = useRef(null);
-
-  useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.play();
-    }
-  }, [videoRef]);
-
   return (
     <div
       className="video-container"
@@ -19,10 +11,15 @@ const Video = () => {
       }}
     >
       <div className="mask-reference"></div>
-      <video autoPlay loop muted playsInline className="video" ref={videoRef}>
-        <source src="/img/video1.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <iframe
+        className="iframe-video"
+        width="500"
+        height="530"
+        src="https://www.youtube.com/embed/CTw1VFeYnHM?autoplay=1&loop=1&mute=1&controls=0&playlist=CTw1VFeYnHM&showinfo=0&rel=0&enablejsapi=1"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 };
